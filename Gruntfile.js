744a0d49777dfc68f5e498bdf845a3e545d2c93e
Антон Домолазов <anton@domelaz.ru>
2016-05-13 10:07:25 +0300

    Сборка css стилей
    grunt less

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
   require("load-grunt-config")(grunt, {
     data: {
       ccVersion: "6",
+      dist: "dist",
       pkg: grunt.file.readJSON("package.json"),
     }
   });
