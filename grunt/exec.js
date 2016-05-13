6e51457bf00318977dec9e2660467b82a05c88ff
Антон Домолазов <anton@domelaz.ru>
2016-05-13 08:46:27 +0300

    Загрузка кода на тестовые машины
    Опциональная фича. Работает, если установлена переменная
    окружения GRUNT_HOSTS=IP:[PORT]
    
    grunt exec:sync

--- grunt/exec.js
+++ grunt/exec.js
@@ -7,6 +7,38 @@
  */
 const isWindows = process.platform === 'win32';
 
+/**
+ * Spread changes on remote machines using rsync
+ *
+ * Export env var before `grunt exec:sync`:
+ * export GRUNT_HOSTS="192.168.0.152[,192.168.0.153[:8011]]"
+ *   ip   -- IP address, mandatory
+ *   port -- optional, default is 8011
+ *
+ * On remote end rsync runs as a daemon with `package.name` stanza
+ */
+let exec = "echo 'No GRUNT_HOSTS exported'"; // No need to sync...
+// ... or harvest remotes
+if (process.env.GRUNT_HOSTS) {
+  const devHosts = process.env.GRUNT_HOSTS.split(',');
+  const commands = devHosts.map(host => {
+    const params = host.split(':');
+    const devHostIp = params[0];
+    const devHostPort = params[1] || 8011;
+    const command = [
+      // @fixme replace `echo` with cygwin path
+      isWindows ? "echo" : "/usr/bin/rsync",
+      "-avz",
+      "--exclude *.swp",
+      `--port ${devHostPort}`,
+      "src/extension/", // @fixme head to dist
+      `${devHostIp}::<%= pkg.name %>`
+    ];
+    return command.join(" ");
+  });
+  exec = commands.join(";");
+}
+
 module.exports = {
   /**
    * Включение/выключение режима отладки CSXS
@@ -16,5 +48,11 @@ module.exports = {
   },
   debugoff: {
     command: isWindows ? "reg delete HKCU\\Software\\Adobe\\CSXS.<%= ccVersion %> /v PlayerDebugMode /f" : "defaults delete com.adobe.CSXS.<%= ccVersion %> PlayerDebugMode"
+  },
+  /**
+   * Синхронизация кода с тестовыми машинами
+   */
+  sync: {
+    command: exec
   },
 };
