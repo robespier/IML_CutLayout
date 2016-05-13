d8dea86c68487f7f6f85ac67aa5978f9aaed6975
Антон Домолазов <anton@domelaz.ru>
2016-05-13 14:11:05 +0300

    CSInterface added

---
+++ typings/adobe/csInterface.d.ts
@@ -0,0 +1,26 @@
+/**
+ * CSInterface - v6.1.0
+ *
+ * !!! MINIMAL !!!
+ */
+
+declare module "CSInterface" {
+  /**
+   * Evaluates a JavaScript script, which can use the JavaScript DOM
+   * of the host application.
+   *
+   * @param {string} script The JavaScript script.
+   * @param {function} [callback] A callback function that receives the result
+   * of execution. If execution fails, the callback function receives the error
+   * message EvalScript_ErrMessage.
+   */
+  interface CSInterfaceInstance {
+    evalScript(script: string, callback?: (result: string) => void): void;
+  }
+
+  interface CSInterfaceConstructor {
+    new(): CSInterfaceInstance;
+  }
+
+  export var CSInterface: CSInterfaceConstructor;
+}
