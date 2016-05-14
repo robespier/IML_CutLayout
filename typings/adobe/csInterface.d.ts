3f53e75b641f6bc8fe032e03a7909b15327755eb
Антон Домолазов <anton@domelaz.ru>
2016-05-14 16:20:08 +0300

    Fix comment position

--- typings/adobe/csInterface.d.ts
+++ typings/adobe/csInterface.d.ts
@@ -5,16 +5,16 @@
  */
 
 declare module "CSInterface" {
-  /**
-   * Evaluates a JavaScript script, which can use the JavaScript DOM
-   * of the host application.
-   *
-   * @param {string} script The JavaScript script.
-   * @param {function} [callback] A callback function that receives the result
-   * of execution. If execution fails, the callback function receives the error
-   * message EvalScript_ErrMessage.
-   */
   interface CSInterfaceInstance {
+    /**
+     * Evaluates a JavaScript script, which can use the JavaScript DOM
+     * of the host application.
+     *
+     * @param {string} script The JavaScript script.
+     * @param {function} [callback] A callback function that receives the result
+     * of execution. If execution fails, the callback function receives the error
+     * message EvalScript_ErrMessage.
+     */
     evalScript(script: string, callback?: (result: string) => void): void;
   }
 
