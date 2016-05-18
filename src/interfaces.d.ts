032f985ae9d53d2307c60d0b077c78eaed7049e9
Антон Домолазов <anton@domelaz.ru>
2016-05-18 17:21:21 +0300

    Интерфейсы для решения

--- src/interfaces.d.ts
+++ src/interfaces.d.ts
@@ -26,3 +26,53 @@ interface CEPResponse {
    */
   status: string;
 }
+
+/**
+ * Точка (1/2 сплайна Безье) в ILST
+ */
+interface IPoint {
+  /**
+   * Point position
+   */
+  anchor: number[];
+
+  /**
+   * In control point
+   */
+  leftPosition: number[];
+
+  /**
+   * Out control point
+   */
+  rightPosition: number[];
+}
+
+/**
+ * Позиция контура в ILST
+ */
+interface IPlacement {
+  /**
+   * Угол наклона оригинального контура
+   */
+  angle: number;
+
+  /**
+   * Верхний левый угол контура (bounding box без stroke, в пунктах)
+   */
+  position: number[];
+}
+
+/**
+ * Формат решения
+ */
+interface ISolution {
+  /**
+   * Область размещения
+   */
+  area: IPoint[];
+
+  /**
+   * Размещения
+   */
+  cuts: IPlacement[];
+}
