ee0bc9c09a0cd468c181076fce5e271439bd27a2
Антон Домолазов <anton@domelaz.ru>
2016-05-18 18:49:34 +0300

    Fake Solver

---
+++ src/panel/serviceSolver.ts
@@ -0,0 +1,28 @@
+/**
+ * Import predefined solutions
+ */
+import { solution as solution1 } from "./3075_eticetka.ai";
+
+const pump = (): ISolution => {
+  return solution1;
+};
+
+const service = ($timeout: ng.ITimeoutService) => {
+  /**
+   * Фейковая реализация Solver;
+   *
+   * Через произвольное время выдаём заранее известный результат
+   */
+  const solve = (data): ng.IPromise<ISolution> => {
+    return $timeout( pump, Math.random() * 10000 );
+  };
+
+  return {
+    solve,
+  };
+};
+
+/**
+ * Отметимся в Ангуляре как сервис
+ */
+angular.module("iml").factory("Solver", ["$timeout", service]);
