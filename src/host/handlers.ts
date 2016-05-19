5d43eb0972c7481a9d7bc0e460378134762c0214
Антон Домолазов <anton@domelaz.ru>
2016-05-19 15:35:31 +0300

    Add `getContour` ILST method

--- src/host/handlers.ts
+++ src/host/handlers.ts
@@ -2,4 +2,5 @@
  * Единая точка входа для методов Иллюстратора, доступных из CEP
  */
 export { applySolution } from "./applySolution";
+export { getContour } from "./getContour";
 export { solution } from "./solution";
