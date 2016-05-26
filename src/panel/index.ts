064591659390448bbe466f1c6cda74ed688b52bb
Антон Домолазов <anton@domelaz.ru>
2016-05-26 16:15:15 +0300

    Redux added

--- src/panel/index.ts
+++ src/panel/index.ts
@@ -1,6 +1,13 @@
 import "angular";
+import "ng-redux";
 
-const app = angular.module("iml", []);
+const app = angular.module("iml", ["ngRedux"]);
+
+app.config([
+  "$ngReduxProvider",
+  ($ngReduxProvider) => {
+    $ngReduxProvider.createStoreWith({});
+}]);
 
 /**
  * После того, как модуль создан, можно заявлять "наши" компоненты.
