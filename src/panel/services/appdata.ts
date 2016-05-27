f90b59b023afb861162d0e772cf6c9c75e0e0280
Антон Домолазов <anton@domelaz.ru>
2016-05-27 18:04:37 +0300

    Local storage in use

--- src/panel/services/appdata.ts
+++ src/panel/services/appdata.ts
@@ -1,9 +1,28 @@
-import { merge } from "lodash";
+import { merge, isEqual } from "lodash";
 import { app } from "../index";
 import { setAppData } from "../actions";
 
-const service = ($ngRedux, config: ICommonConfig) => {
+const service = ($ngRedux, storage, config: ICommonConfig) => {
+  /**
+   * At init stage keep app defaults from config
+   * Later used to track changes against app state
+   */
   const appDefaults = merge({}, config.defaults);
+
+  /**
+   * This fields from state is persistent:
+   */
+  const storable = ["materials"];
+
+  /**
+   * Merge defaults with storable data
+   */
+  storable.forEach(key => {
+    const record = storage.get(key);
+    if (record) {
+      appDefaults[key] = JSON.parse(record);
+    }
+  });
 
   /**
    * Map service methods to state actions
@@ -21,11 +40,28 @@ const service = ($ngRedux, config: ICommonConfig) => {
 
   service.setAppData(appDefaults);
 
+  /**
+   * When someone update the state -- sync storable data with localStorage
+   */
+  const syncStorage = () => {
+    const state = <IRootReducer>$ngRedux.getState();
+    storable.forEach(key => {
+      const newValue = state.ui[key];
+      if (!isEqual(appDefaults[key], newValue)) {
+        storage.set(key, JSON.stringify(newValue));
+        appDefaults[key] = newValue;
+      }
+    });
+  };
+
+  $ngRedux.subscribe(syncStorage);
+
   return service;
 };
 
 app.factory("AppData", [
   "$ngRedux",
+  "localStorageService",
   "CommonConfig",
   service,
 ]);
